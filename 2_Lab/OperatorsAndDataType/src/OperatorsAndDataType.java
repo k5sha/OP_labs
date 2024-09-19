855263d0550bbd6bddf09c345938f9f0599041cc
Artur Iliushchits <arturiliushchits@gmail.com>
2024-09-19 10:06:44 +0300

    Update OperatorsAndDataType.java

--- 2_Lab/OperatorsAndDataType/src/OperatorsAndDataType.java
+++ 2_Lab/OperatorsAndDataType/src/OperatorsAndDataType.java
@@ -1,32 +1,50 @@
+/**
+ *         Math.pow() - Степінь
+ *         Math.E - Експонента
+ *         Math.log10() - Десятковий логарифм
+ *         Math.sqrt() - Корінь квадратний
+ *         Math.abs() - Модуль
+ *         Math.atan() - Арктангенс
+ *         Math.cosh() -  Гіперболічний косинус
+ *         Math.sinh() -  Гіперболічний синус
+ */
+
 public class OperatorsAndDataType {
 
-    //    Math.pow() - Степінь
-    //    Math.E - Експонента
-    //    Math.log10() - Десятковий логарифм
-    //    Math.sqrt() - Корінь квадратний
-    //    Math.abs() - Модуль
-    //    Math.atan() - Арктангенс
-
     public static double task_2(double a, double b, double c, double d) {
-        return  (((Math.pow(Math.E,a) + (3 * Math.log10(c))) / (Math.sqrt(Math.pow(b,c)))) * Math.abs(Math.atan(d)));
+        return  ( (Math.pow(Math.E,a) + 3 * Math.log10(c)) / Math.sqrt(Math.pow(b,c)) ) * Math.abs(Math.atan(d));
     }
 
-    //   Math.cosh() -  Гіперболічний косинус
-    public  static  double task_12(double a, double b, double c, double d) {
-        return  (6 * (Math.pow(Math.sin(Math.abs(2 * a)), Math.log10(b)))) + Math.sqrt((c * Math.cosh(-d)));
+    public static double task_12(double a, double b, double c, double d) {
+        return 6 * Math.pow(Math.sin(Math.abs(2 * a)), Math.log10(b)) + Math.sqrt(c * Math.cosh(-d));
     }
 
-    //    Math.sinh() -  Гіперболічний синус
-    public  static  double task_22(double a, double b, double c, double d) {
-        return Math.pow((4 * Math.sinh(Math.sqrt(Math.abs((a / b)))) + (3 * Math.asin(c))), d);
+    public static double task_22(double a, double b, double c, double d) {
+        return Math.pow(4 * Math.sinh(Math.sqrt(Math.abs(a / b))) + 3 * Math.asin(c), d);
+    }
+
+    public static void printResult(double result){
+        if(Double.isNaN(result))
+            System.out.println("Не можливо обрахувати, змініть вхідні параметри");
+        else
+            System.out.println(result);
     }
 
     public static void main(String[] args) {
         //  Task 2
-        System.out.println(task_2(2.34, 0.756, 2.23, -1.653));
+        double result = task_2(2.34, 0.756, 2.23, -1.653);
+        printResult(result);
+
         //  Task 12
-        System.out.println(task_12(1.478, 9.26, 0.68, 2.24));
+        result = task_12(1.478, 9.26, 0.68, 2.24);
+        printResult(result);
+
         //  Task 22
-        System.out.println(task_22(1.23, -0.34, 0.707, 2.312));
+        result = task_22(1.23, -0.34, 0.707, 2.312);
+        printResult(result);
+
+        //  Task 22 - Test case for NaN result
+        result = task_12(0, 0, -10, 0);
+        printResult(result);
     }
 }

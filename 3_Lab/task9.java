ec182e5cabde209ba13598462611bc26ed4732f2
Artur Iliushchits <arturiliushchits@gmail.com>
2024-09-26 21:34:41 +0300

    Update task9.java

--- 3_Lab/task9.java
+++ 3_Lab/task9.java
@@ -1,11 +1,37 @@
 import static java.lang.Math.sqrt;
 
+/**
+ *      У цьому рішенні оскільки формула суми не містила "інкрементуючу змінну",
+ *      то краще не використовувати цикл for і натомість скористатися простою
+ *      властивістю з математики:
+ *              a + a + a + a...... = n * (a)
+ *              ↑_____n разів_____↑
+ *
+ *      Тобто замість циклу for краще множення, це використовує менше
+ *      ресурсів (хоча для такої задачі це майже не помітно,
+ *      але читабельність коду краще).
+ *
+ *      Було створено також реалізацію з використанням for у файлі
+ *      task9_alternative.java
+ *
+ *      Результат виконання у реалізації task9 співпадає з task9_alternative.
+ */
+
 public class task9 {
 
+    /**
+     * Допоміжний метод, який перевіряє число на парність
+     */
     private static boolean isEven(double number){
         return number % 2 == 0;
     }
 
+    /**
+     * Здійснення обрахунків для випадку непарного l.
+     * Сума (t разів) коренів добутку t і l
+     *
+     * @return якщо t * l менше нуля  → помилка, інакше → результат
+     */
     private static double oddSum(double t, double l){
         if(t * l < 0)
             throw new IllegalArgumentException("t multiplied by l must be more or equal to zero (when l is odd)");
@@ -13,6 +39,12 @@ public class task9 {
         return sqrt(t * l) * t;
     }
 
+    /**
+     * Здійснення обрахунків для випадку парного l.
+     * Сума (t разів) коренів добутку t і l
+     *
+     * @return якщо t менший або рівний нулю → помилка, інакше → результат
+     */
     private static double evenSum(double t, double l){
         if(t <= 0)
             throw new IllegalArgumentException("t must be more than zero (when l is even)");
@@ -20,10 +52,20 @@ public class task9 {
         return ( l / sqrt(t) ) * t;
     }
 
+    /**
+     * Метод, що дає розв'язок на завдання 9.
+     *
+     * Як на мене, тернарний оператор виглядає дуже зручно в цьому випадку.
+     * Це набагато краще ніж кілька строк з if-ом.
+     */
     public static double calculate(double t, double l){
         return isEven(l) ? evenSum(t, l) : oddSum(t, l);
     }
 
+    /**
+     * Допоміжний метод, що виводить у консоль
+     * вхідні аргументи, результат і обробляє помилки
+     */
     public static void printResult(double t, double l){
         System.out.print("t:" + t + "\t\t\tl:" + l + "\t\t\tresult: ");
 
@@ -51,7 +93,7 @@ public class task9 {
         printResult(1, Double.MAX_VALUE);
         printResult(1, Double.NaN);
 
-        // additional use-case (oddSum wrong input parameters)
+        // additional use-case (calls oddSum with wrong input parameters)
         printResult(-1, 9);
     }
 }
